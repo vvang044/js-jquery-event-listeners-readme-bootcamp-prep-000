919231aebeba8acdd8f17bd1ce53f1379f6d003f
Vik Vangala <vvang044@gmail.com>
2019-06-17 17:54:31 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,9 +12,9 @@ function frameIt(){
 }
 
 function pressIt(){
-  $('form').on('keydown', function(key){
+  $('input').on('keydown', function(key){
     if(key.which === 71){
-      alter("You pressed G")
+      alert("You pressed G")
     }
   })
 }
