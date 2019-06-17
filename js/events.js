76e6624629d828fbb24514111901c7be9055ab39
Vik Vangala <vvang044@gmail.com>
2019-06-17 17:39:22 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,13 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!");
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
 
 });
